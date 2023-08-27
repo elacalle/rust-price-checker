493b67d4878d97bb49e92051139719366058718e
Nesto <hello@nesto.es>
2023-08-27 20:44:47 +0200

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,23 @@
-use std::{fs::File, io::Write, collections::HashMap};
+use scraper::{Html, Selector};
 
-use headless_chrome::{Browser, protocol::cdp::Page};
+mod scrapper;
+
+use crate::scrapper::spider::create;
 
 fn main() {
-    let url = "";
-    let browser = Browser::default().expect("BROWSER_INIT_FAILED");
+  let mut scrapper = create();
+  scrapper.setup();
+  
+  match scrapper.run("") {
+    Ok(body) => {
+      let document = Html::parse_document(&body);
+      let selector = Selector::parse("#priceBlock").expect("INVALID SELECTOR");
 
-    let tab = browser.new_tab().expect("TAB_INIT_FAILED");
+      let html = document.select(&selector).next().unwrap();
 
-    let mut headers = HashMap::new();
-    let mut attributes = HashMap::new();
+      print!("{}", html.value().attr("data-price").unwrap())
 
-    headers.insert("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36");
-
-    tab.set_extra_http_headers(headers).expect("HEADER_ERROR");
-
-    tab.navigate_to(url).expect("NAVIGATION_FAILED");
-
-    tab.wait_until_navigated().expect("NAVIGATION_FAILED");
-
-    let element = tab.find_element("#priceBlock").unwrap();
-    
-    let attrs = element.get_attributes().expect("ATTRS_NOT_FOUND");
-    let attribute_result = attrs.expect("ATTRS_EMPTY");
-    
-    let attributes_amount = attribute_result.len() / 2;
-
-    for n in 0..attributes_amount {
-      let name = attribute_result.get(n * 2).expect("unknown name");
-      let value = attribute_result.get(n * 2 + 1).expect("unknown value");
-
-
-      print!("clave: {} y valor {}\n", name, value);
-
-      attributes.insert(name.as_str(), value.as_str());
-    }
-
-
-    println!("{:?}", attributes.get("data-price").expect("UNKNOWN PRICE"));
+    },
+    Err(_e) => print!("SCRAPPER_ERROR")
+  }
 }
