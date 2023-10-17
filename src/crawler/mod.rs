7302af202e6f85608ebe661cf89700c365e88d21
Ernesto <ernestohlacalle@gmail.com>
2023-10-17 23:05:08 +0200

    wip

--- src/crawler/mod.rs
+++ src/crawler/mod.rs
@@ -1,6 +1,10 @@
 pub mod spider {
+    use headless_chrome::protocol::cdp::Page::CaptureScreenshotFormatOption;
     use headless_chrome::Browser;
     use std::collections::HashMap;
+    use std::fs::File;
+    use std::io::Write;
+    use std::process;
 
     const USER_AGENT: &str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36";
 
@@ -23,15 +27,50 @@ pub mod spider {
 
             match new_tab.navigate_to(url)?.wait_until_navigated() {
                 Ok(page) => {
+                    let result = page.capture_screenshot(
+                        CaptureScreenshotFormatOption::Png,
+                        Some(75),
+                        None,
+                        true,
+                    )?;
                     return page.get_content();
                 }
                 Err(e) => return Err(e),
             }
         }
+
+        pub fn screenshot(&self, url: &str) -> () {
+            let new_tab = self.browser.new_tab().unwrap();
+            new_tab
+                .set_extra_http_headers(self.headers.clone())
+                .unwrap();
+
+            //match new_tab
+            //    .navigate_to(url)
+            //    .expect("invalid url")
+            //    .wait_until_navigated()
+            //{
+            //    Ok(page) => {
+            //        match page.capture_screenshot(
+            //            CaptureScreenshotFormatOption::Png,
+            //            Some(75),
+            //            None,
+            //            true,
+            //        ) {
+            //            Ok(data) => {
+            //                let mut file = File::create("foo.png");
+            //                file.expect("archivo no encontrado").write_all(&data);
+            //            }
+            //            _ => process::exit(1),
+            //        }
+            //    }
+            //    Err(e) => process::exit(1),
+            //}
+        }
     }
 
     pub fn create() -> Worker {
-        let headless_browser = Browser::default().expect("BROWSER_INIT_FAILED");
+        let headless_browser = Browser::default().unwrap();
 
         Worker {
             user_agent: &USER_AGENT,
--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,45 @@
 mod crawler;
 mod scrapper;
-
 use crate::crawler::spider;
 use crate::scrapper::parser;
-use std::{env, process};
+use fantoccini;
+use std::{env, fs::File, io::Write, process};
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let commands: Vec<String> = env::args().collect();
 
-    match commands.get(1) {
-        Some(value) => bot(value),
-        None => {
-            print!("INVALID_URL")
-        }
-    }
+    println!("PASA");
+
+    bot("").await;
+
+    println!("CARGADO")
+    //match commands.get(1) {
+    //    Some(value) => bot(value),
+    //    None => {}
+    //}
 }
 
-fn bot(url: &str) {
-    let mut scrapper = spider::create();
+async fn bot(url: &str) {
+    println!("CRATE SPIDER");
 
-    scrapper.setup();
+    let mut caps = serde_json::map::Map::new();
+    let opts = serde_json::json!({
+        "args": ["--headless", "--disable-gpu", "--no-sandbox", "--disable-dev-shm-usage", "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"],
+    });
+    caps.insert("goog:chromeOptions".to_string(), opts);
 
-    match scrapper.run(url) {
-        Ok(body) => {
-            let worker: parser::Worker = parser::create(&body);
-            match worker.run() {
-                Ok(value) => {
-                    println!("{}", value)
-                }
-                Err(_e) => {
-                    process::exit(1);
-                }
-            }
-        }
-        Err(_e) => {
-            process::exit(1);
-        }
-    }
+    let c = fantoccini::ClientBuilder::native()
+        .capabilities(caps)
+        .connect("http://localhost:4444")
+        .await
+        .expect("Error");
+
+    c.goto("https://www.pccomponentes.com/msi-modern-14-c13m-426xes-intel-core-i7-1355u-16gb-1tb-ssd-14").await;
+
+    let value = c.screenshot().await.unwrap();
+
+    let mut file = File::create("foo.png").unwrap();
+    file.write_all(&value);
+    // file.expect("archivo no encontrado")w(&value);
 }
