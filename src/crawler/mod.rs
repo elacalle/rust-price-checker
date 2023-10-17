1e51cd7e4771fe98b5a1289bbe6201fb7d6f7916
Ernesto <ernestohlacalle@gmail.com>
2023-10-17 23:18:07 +0200

    delete headless_chrome

--- src/crawler/mod.rs
+++ src/crawler/mod.rs
@@ -1,6 +1,4 @@
 pub mod spider {
-    use headless_chrome::protocol::cdp::Page::CaptureScreenshotFormatOption;
-    use headless_chrome::Browser;
     use std::collections::HashMap;
     use std::fs::File;
     use std::io::Write;
@@ -11,7 +9,7 @@ pub mod spider {
     pub struct Worker {
         user_agent: &'static str,
         headers: HashMap<&'static str, &'static str>,
-        browser: Browser,
+        // browser: Browser,
     }
 
     impl Worker {
@@ -19,62 +17,14 @@ pub mod spider {
             self.headers.insert("user-agent", self.user_agent);
         }
 
-        pub fn run(&self, url: &str) -> Result<std::string::String, anyhow::Error> {
-            let new_tab = self.browser.new_tab().unwrap();
-            new_tab
-                .set_extra_http_headers(self.headers.clone())
-                .unwrap();
+        pub fn run(&self, url: &str) -> () {}
 
-            match new_tab.navigate_to(url)?.wait_until_navigated() {
-                Ok(page) => {
-                    let result = page.capture_screenshot(
-                        CaptureScreenshotFormatOption::Png,
-                        Some(75),
-                        None,
-                        true,
-                    )?;
-                    return page.get_content();
-                }
-                Err(e) => return Err(e),
-            }
-        }
-
-        pub fn screenshot(&self, url: &str) -> () {
-            let new_tab = self.browser.new_tab().unwrap();
-            new_tab
-                .set_extra_http_headers(self.headers.clone())
-                .unwrap();
-
-            //match new_tab
-            //    .navigate_to(url)
-            //    .expect("invalid url")
-            //    .wait_until_navigated()
-            //{
-            //    Ok(page) => {
-            //        match page.capture_screenshot(
-            //            CaptureScreenshotFormatOption::Png,
-            //            Some(75),
-            //            None,
-            //            true,
-            //        ) {
-            //            Ok(data) => {
-            //                let mut file = File::create("foo.png");
-            //                file.expect("archivo no encontrado").write_all(&data);
-            //            }
-            //            _ => process::exit(1),
-            //        }
-            //    }
-            //    Err(e) => process::exit(1),
-            //}
-        }
+        pub fn screenshot(&self, url: &str) -> () {}
     }
 
     pub fn create() -> Worker {
-        let headless_browser = Browser::default().unwrap();
-
         Worker {
             user_agent: &USER_AGENT,
-            browser: headless_browser,
             headers: HashMap::new(),
         }
     }
